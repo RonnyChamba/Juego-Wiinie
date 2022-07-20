e72c97c2cfed42e09fa1cdc0cd14c84d8aad996b
Ronny <ronnychamba96@gmail.com>
2022-07-20 00:58:40 -0500

    conexion firebase-registro jugador

--- app/src/main/java/com/example/juegowoniiepooh/MainActivity.java
+++ app/src/main/java/com/example/juegowoniiepooh/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.juegowoniiepooh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin, btnRegistro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegistro = findViewById(R.id.btnRegistro);
+
+        Typeface typeface = Typeface.createFromAsset(MainActivity.this.getAssets(), "fuentes/zombie.TTF");
+        btnLogin.setTypeface(typeface);
+        btnRegistro.setTypeface(typeface);
+
+        btnLogin.setOnClickListener( (event) -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btnRegistro.setOnClickListener( (event) -> {
+            Intent intent = new Intent(this, Registro.class);
+            startActivity(intent);
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/juegowoniiepooh/Registro.java
@@ -0,0 +1,128 @@
+package com.example.juegowoniiepooh;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Registro extends AppCompatActivity {
+
+    EditText tvEmail, tvPassword, tvNombre;
+    Button btnRegistrar;
+    FirebaseAuth auth;
+    TextView txtFecha, tvTitulo;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        tvTitulo = findViewById(R.id.txtTituloRegistro);
+
+        tvEmail = findViewById(R.id.txtCorreo);
+        tvNombre = findViewById(R.id.txtNombre);
+        tvPassword = findViewById(R.id.txtPassword);
+        txtFecha = findViewById(R.id.txtFecha);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        auth = FirebaseAuth.getInstance();
+        Date date = new Date();
+        SimpleDateFormat fecha = new SimpleDateFormat("d 'de' MMMM 'del'  yyyy");
+        String stringFecha = fecha.format(date);
+        txtFecha.setText(stringFecha);
+        Typeface fuente = Typeface.createFromAsset(Registro.this.getAssets(), "fuentes/zombie.TTF");
+        tvTitulo.setTypeface(fuente);
+        tvEmail.setTypeface(fuente);
+        tvPassword.setTypeface(fuente);
+        tvNombre.setTypeface(fuente);
+        txtFecha.setTypeface(fuente);
+        btnRegistrar.setTypeface(fuente);
+
+        btnRegistrar.setOnClickListener( (event)->{
+
+            String email = tvEmail.getText().toString();
+            String password = tvPassword.getText().toString();
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                tvEmail.setError("Email invalido");
+                tvEmail.setFocusable(true);
+
+            } else if (password.trim().length() < 6) {
+                tvPassword.setError("Constraseña debe ser mayor a 6");
+                tvPassword.setFocusable(true);
+            }else {
+                registrarJugador(email, password);
+            }
+        });
+
+    }
+
+    private void registrarJugador(String email, String password){
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    FirebaseUser user = auth.getCurrentUser();
+                    int contador = 0;
+                    assert  user !=null;
+
+                    String uidString = user.getUid();
+                    String nombreString = tvNombre.getText().toString();
+                    String fechaString = txtFecha.getText().toString();
+                    String emailString = tvEmail.getText().toString();
+                    String passwordString = tvPassword.getText().toString();
+                    Map<Object, Object> datosJugador = new HashMap<>();
+                    datosJugador.put("Uid", uidString);
+                    datosJugador.put("Email", emailString);
+                    datosJugador.put("Password", passwordString);
+                    datosJugador.put("Nombres", nombreString);
+                    datosJugador.put("Fecha", fechaString);
+                    datosJugador.put("Zombies", contador);
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+                    DatabaseReference reference = database.getReference("MI DATA BASE JUGADORES");
+
+                    reference.child(uidString).setValue(datosJugador);
+                    startActivity(new Intent(Registro.this, Menu.class));
+                    Toast.makeText(Registro.this, "Jugador creado", Toast.LENGTH_LONG).show();
+                    finish();
+                }else {
+                    Toast.makeText(Registro.this, "Ha ocurrido un error " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Registro.this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
